1fbb91c701d8409570b543cf27e233d58615a212
RobertDupu <robert_dupu@yahoo.com>
2017-01-04 13:52:21 +0200

    Add files via upload

---
+++ snakeEasy.cpp
@@ -0,0 +1,340 @@
+//by Dupu Robert
+#include <iostream>
+#include <ctime>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+time_t TimeUp,TimeStart;
+bool tie,gameOver,Lose;char reset;
+const int width=24;
+const int height=24;
+const int timeGame=60;
+int Pscore,AIscore,CanIgo[100][100];
+int lengthTail,AIlengthTail;
+struct coordinates{
+int x;
+int y;
+};
+coordinates head,tail[580],fruit,AIhead,AItail[580];
+enum Directon { STOP = 0, LEFT, RIGHT, UP, DOWN};
+Directon Goes;
+void TimeON()
+{
+    time(&TimeUp);
+         TimeStart=TimeUp;
+}
+void PutObject(coordinates &object)
+{
+
+        object.x=rand()%(width-1)+1;
+	    object.y=rand()%(height-1)+1;
+	    if (head.x==object.x&&head.y==object.y)
+           PutObject(object);
+
+        else
+      for (int i=0;i<=lengthTail;i++)
+    if (tail[i].x==object.x&&tail[i].y==object.y)
+       PutObject(object);
+
+}
+void Setup()
+{
+
+	gameOver=false;
+	tie=false;
+	Goes=STOP;
+	head.x=width/2;
+	head.y=height/2;
+	PutObject(fruit);
+    Pscore=AIscore=0;
+    AIlengthTail=0;
+    lengthTail=0;
+    AIhead.x=width/2-5;
+	AIhead.y=height/2;
+}
+char Arena[100][100];
+void Draw()
+{
+	system("cls");
+for(int i=0;i<=width;i++)
+    for(int j=0;j<=height;j++)
+    if(i==0||j==0||i==width||j==height)
+        Arena[i][j]='#';
+        else
+        Arena[i][j]=' ';
+
+	Arena[head.y][head.x]='O';
+    Arena[AIhead.y][AIhead.x]='X';
+    Arena[fruit.y][fruit.x]='F';
+
+	for(int i=1;i<=lengthTail;i++)
+        Arena[tail[i].y][tail[i].x]='o';
+    for(int i=1;i<=AIlengthTail;i++)
+        Arena[AItail[i].y][AItail[i].x]='x';
+
+
+        cout<<"time: "<<timeGame-(TimeUp-TimeStart)<<endl;
+	for(int i=0;i<width+1;i++)
+    {for(int j=0;j<height+1;j++)
+       cout<<Arena[i][j];
+       cout<<endl;
+    }
+	cout<<endl;
+	cout<<"Score:"<<Pscore<<endl;
+	cout<<"ScoreAI:"<<AIscore<<endl;
+
+}
+void Input()
+{
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a':
+		    if(Goes!=RIGHT)
+			Goes=LEFT;
+			break;
+		case 'd':
+		    if(Goes!=LEFT)
+			Goes=RIGHT;
+			break;
+		case 'w':
+		    if(Goes!=DOWN)
+			Goes=UP;
+			break;
+		case 's':
+		    if(Goes!=UP)
+			Goes=DOWN;
+			break;
+		case 'k':
+			gameOver=true;
+			break;
+
+
+		}
+	}
+
+}
+void Teleport(coordinates &B)
+	{if (B.x>=width)
+        B.x=1;
+    else
+       if (B.x<=0)
+         B.x=width-1;
+	if (B.y>=height)
+	     B.y=1;
+           else
+    if (B.y<=0)
+     B.y=height - 1;}
+void MovmentTail(coordinates &headSnake,coordinates tailSnake[],int lengthTailSnake)
+{
+coordinates prev,aux;
+	prev.x=tailSnake[0].x;
+	prev.y=tailSnake[0].y;
+	tailSnake[0].x=headSnake.x;
+	tailSnake[0].y=headSnake.y;
+	for (int i=0;i<=lengthTailSnake;i++)
+	{
+		aux.x=tailSnake[i].x;
+		aux.y=tailSnake[i].y;
+		tailSnake[i].x=prev.x;
+		tailSnake[i].y=prev.y;
+		prev.x=aux.x;
+		prev.y=aux.y;
+	}
+}
+void MovmentPlayer()
+{
+    switch (Goes)
+	{
+	case LEFT:
+		head.x--;
+		break;
+	case RIGHT:
+		head.x++;
+		break;
+	case UP:
+		head.y--;
+		break;
+	case DOWN:
+		head.y++;
+		break;
+	default:
+		break;
+	}
+}
+void AIeyes()
+{
+   for(int i=0;i<=width;i++)
+    for(int j=0;j<=height;j++)
+        CanIgo[i][j]=1;
+
+
+        CanIgo[head.y][head.x]=0;
+        CanIgo[head.y-1][head.x-1]=0;
+        CanIgo[head.y-1][head.x+1]=0;
+        CanIgo[head.y-1][head.x]=0;
+        CanIgo[head.y+1][head.x]=0;
+        CanIgo[head.y+1][head.x-1]=0;
+        CanIgo[head.y+1][head.x+1]=0;
+        CanIgo[head.y][head.x-1]=0;
+        CanIgo[head.y][head.x+1]=0;
+        for(int i=1;i<=lengthTail;i++)
+        CanIgo[tail[i].y][tail[i].x]=0;
+    for(int i=1;i<=AIlengthTail;i++)
+        CanIgo[AItail[i].y][AItail[i].x]=0;
+}
+void favorabileCase(bool &AImove)
+{
+    AImove=false;
+     if(AIhead.x<fruit.x&&AIhead.x+1!=AItail[0].x&&CanIgo[AIhead.y][AIhead.x+1]==1)
+            {AIhead.x++;AImove=true;}
+            else
+        if(AIhead.x>fruit.x&&AIhead.x-1!=AItail[0].x&&CanIgo[AIhead.y][AIhead.x-1]==1)
+            {AIhead.x--;AImove=true;}
+            else
+        if(AIhead.y<fruit.y&&AIhead.y+1!=AItail[0].y&&CanIgo[AIhead.y+1][AIhead.x]==1)
+            {AIhead.y++;AImove=true;}
+            else
+        if(AIhead.y>fruit.y&&AIhead.y-1!=AItail[0].y&&CanIgo[AIhead.y-1][AIhead.x]==1)
+            {AIhead.y--;AImove=true;}
+
+
+}
+void NefavorabileCase()
+{
+    int GORIGHT = CanIgo[AIhead.y][AIhead.x+1];
+    int GOLEFT  = CanIgo[AIhead.y][AIhead.x-1];
+    int GOUP    = CanIgo[AIhead.y-1][AIhead.x];
+    int GODOWN  = CanIgo[AIhead.y+1][AIhead.x];
+
+    if(GORIGHT==0)
+     {if(GOLEFT==0)
+     {if(GODOWN==0)
+      AIhead.y--;
+    else
+      AIhead.y++;}
+      else
+      AIhead.x--;}
+      else
+      AIhead.x++;
+
+}
+void MovementAI(coordinates &AIhead)
+{
+    bool AImove;
+     favorabileCase(AImove);
+     if(AImove==false)
+        NefavorabileCase();
+
+
+}
+addTail(int &score,coordinates headSnake,int &lengthTailSnake)
+{if (headSnake.x==fruit.x && headSnake.y==fruit.y)
+	{
+
+		score+=10;
+        lengthTailSnake++;
+		PutObject(fruit);
+
+	}
+
+	}
+void EndGameCondition()
+{
+  if(head.x==AIhead.x&&head.y==AIhead.y)
+  {
+      gameOver=true;
+       tie=true;
+  }
+  else
+
+  {
+    for (int i=0;i<=lengthTail;i++)
+		if(tail[i].x==head.x&&tail[i].y==head.y)
+			{gameOver=true;Lose=true;}
+    for (int i=0;i<=AIlengthTail;i++)
+		if (AItail[i].x==head.x&&AItail[i].y==head.y)
+            {gameOver=true;Lose=true;}
+    for (int i=0;i<=lengthTail;i++)
+		if (tail[i].x==AIhead.x&&tail[i].y==AIhead.y)
+			{gameOver=true;Lose=false;}
+    for (int i=0;i<=AIlengthTail;i++)
+    	if (AItail[i].x==AIhead.x&&AItail[i].y==AIhead.y)
+            {gameOver=true;Lose=false;}
+  }
+  if(TimeUp-TimeStart>=timeGame)
+  {
+      gameOver=true;
+      if(Pscore>AIscore)
+        Lose=false;
+      else
+        Lose=true;
+  }
+  else
+    time(&TimeUp);
+}
+void Logic()
+{
+    AIeyes();
+    MovmentTail(head,tail,lengthTail);
+     MovmentTail(AIhead,AItail,AIlengthTail);
+
+	MovmentPlayer();
+	if(Goes!=STOP)
+    MovementAI(AIhead);
+	Teleport(head);
+	Teleport(AIhead);
+    EndGameCondition();
+
+     addTail(Pscore,head,lengthTail);
+	 addTail (AIscore,AIhead,AIlengthTail);
+}
+void startGame()
+{
+    Draw();
+ while(Goes==STOP)
+    {
+    Input();}
+    TimeON();
+}
+void PLay()
+{
+    Setup();
+    startGame();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+		Sleep(80);
+	}
+}
+int main()
+{
+     RESET:
+
+     PLay();
+if(tie)
+cout<<"eh";
+else
+if(Lose==true)
+    cout<<"lacrima";
+else
+    cout<<"GG"<<endl;
+    cout<<endl;
+    do{
+    cout<<"mai vrei? d/n";
+     cin>>reset;
+
+
+    }while(reset!='d'&&reset!='n');
+    if(reset=='d')
+        goto RESET;
+
+
+
+
+
+	return 0;
+}
